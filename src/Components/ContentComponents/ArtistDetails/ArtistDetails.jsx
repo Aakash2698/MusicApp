f36e867903cadf5062227c0d9333bc4529f70a11
Aakash Suryawanshi <aakash.suryawanshi@iblinfotech.com>
2021-01-26 18:32:22 +0530

    change home page mobile design

--- src/Components/ContentComponents/ArtistDetails/ArtistDetails.jsx
+++ src/Components/ContentComponents/ArtistDetails/ArtistDetails.jsx
@@ -14,6 +14,7 @@ import {
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import axios from "axios";
+
 class ArtistDetails extends Component {
   state = {
     tabValue: "topSongs",
